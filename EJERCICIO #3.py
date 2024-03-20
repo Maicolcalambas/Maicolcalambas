4f54b5e86de82eab643037116deb01528c8b9c75
Maicolcalambas <161541545+Maicolcalambas@users.noreply.github.com>
2024-03-19 20:59:03 -0500

    Add files via upload

---
+++ EJERCICIO #3.py
@@ -0,0 +1,11 @@
+sabe_ingles = True
+sabe_python = False
+
+if sabe_ingles and sabe_python:
+    print("Cumples con los requisitos para postularte")
+elif sabe_ingles:
+    print("Necesitas saber programar en Python para postularte, y tener conocimientos de ingles")
+elif sabe_python:
+    print(" Para postularte necesitas tener conocimiento de inglés.")
+else:
+    print("Necesitas saber programar en Python para postularte.")
